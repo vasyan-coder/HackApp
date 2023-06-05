0718042730dde1d94ff45cf88e1217370729c0b1
Vasyan <vasyan.coder@gmail.com>
2023-06-05 17:22:44 +0300

    feat: added domain layer

---
+++ feature_login/domain/src/main/java/com/vasyancoder/feature_login/domain/repository/UserRepository.kt
@@ -0,0 +1,20 @@
+package com.vasyancoder.feature_login.domain.repository
+
+import com.vasyancoder.feature_login.domain.use_case.AuthenticateUserUseCase
+
+interface UserRepository {
+
+    /**
+     * Verifies account information
+     */
+    suspend fun authenticateUser(
+        username: String,
+        password: String
+    ): AuthenticateUserUseCase.AuthenticationResult
+
+    /**
+     * Stores a login in the SharedPref
+     * that says that the authorization was successful
+     */
+    fun storeUserSession(userId: String)
+}
