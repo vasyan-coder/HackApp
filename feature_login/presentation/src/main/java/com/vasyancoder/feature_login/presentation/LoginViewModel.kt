a0c96c89c3985b66b542cc35d5be6c9155ee6889
Vasyan <vasyan.coder@gmail.com>
2023-06-05 18:36:56 +0300

    feat: added authorization

---
+++ feature_login/presentation/src/main/java/com/vasyancoder/feature_login/presentation/LoginViewModel.kt
@@ -0,0 +1,31 @@
+package com.vasyancoder.feature_login.presentation
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.vasyancoder.data.RepositoryImpl
+import com.vasyancoder.feature_login.domain.use_case.AuthenticateUserUseCase
+import com.vasyancoder.feature_login.domain.use_case.ValidateUserInputUseCase
+import kotlinx.coroutines.launch
+
+class LoginViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val repository = RepositoryImpl(application)
+    private val validateUserInput = ValidateUserInputUseCase()
+    private val authenticateUser = AuthenticateUserUseCase(repository)
+
+    private val _authenticationResult =
+        MutableLiveData<AuthenticateUserUseCase.AuthenticationResult>()
+    val authenticationResult: LiveData<AuthenticateUserUseCase.AuthenticationResult> =
+        _authenticationResult
+
+    fun authenticateUser(login: String, password: String) {
+        viewModelScope.launch {
+            val authentication =
+                authenticateUser(AuthenticateUserUseCase.UserCredentials(login, password))
+            _authenticationResult.postValue(authentication)
+        }
+    }
+}
